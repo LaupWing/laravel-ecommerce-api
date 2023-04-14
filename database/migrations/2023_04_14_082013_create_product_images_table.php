deb568d9c9cfa054f05d5c41964063f075f1c228
LaupWing <loc_strict@hotmail.com>
2023-04-14 11:23:21 +0200

    udpate

--- database/migrations/2023_04_14_082013_create_product_images_table.php
+++ database/migrations/2023_04_14_082013_create_product_images_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
          $table->string("url");
          $table->boolean("is_primary")->default(false);
          $table->timestamps();
-         // $table->unique(["product_id", "is_primary"]);
+         $table->unique(["product_id", "is_primary"], "unique_product_id_primary_image");
+
       });
    }
 
