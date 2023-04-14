28f3f6b428e1fa88aeb074cc34d4a623c817ebc8
LaupWing <loc_strict@hotmail.com>
2023-04-14 10:34:29 +0200

    udpate

--- database/migrations/2023_04_11_082013_create_product_images_table.php
+++ database/migrations/2023_04_11_082013_create_product_images_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
    {
       Schema::create('product_images', function (Blueprint $table) {
          $table->id();
+         $table->foreignId("product_id")->constrained()->onDelete("cascade");
+         $table->string("image_path");
+         $table->boolean("is_primary")->default(false);
          $table->timestamps();
+
+         $table->unique(["product_id", "is_primary"]);
       });
    }
 
