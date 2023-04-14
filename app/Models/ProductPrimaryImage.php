65b36f086d5607e78e3cc5641261cbd9c7b8e33f
LaupWing <loc_strict@hotmail.com>
2023-04-14 12:06:21 +0200

    udpate

--- app/Models/ProductPrimaryImage.php
+++ app/Models/ProductPrimaryImage.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class ProductPrimaryImage extends Model
 {
    use HasFactory;
+
+   protected $fillable = ["product_id", "product_image_id"];
+
+   public function product() {
+      return $this->belongsTo(Product::class);
+   }
+
+   public function productImage() {
+      return $this->belongsTo(ProductImage::class);
+   }
 }
