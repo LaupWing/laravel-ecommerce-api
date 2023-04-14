e4fcb0aed868fe1bc484e4507cfe8e811623f4a9
LaupWing <loc_strict@hotmail.com>
2023-04-14 10:58:19 +0200

    update

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -15,7 +15,21 @@ class ProductImage extends Model
       "is_primary"
    ];
 
-   public function product(){
+   public function product()
+   {
       return $this->belongsTo(Product::class);
+   }
+
+   public function setPrimary(bool $primary): void
+   {
+      if ($primary) {
+         // Unset primary flag for all other images of this product
+         ProductImage::where("product_id", $this->product_id)
+            ->where("id", "<>", $this->id)
+            ->update(["is_primary" => false]);
+      }
+
+      $this->is_primary = $primary;
+      $this->save();
    }
 }
