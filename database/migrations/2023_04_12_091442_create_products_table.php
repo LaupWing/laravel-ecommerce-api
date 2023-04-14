8cd508571469f0b1d921bcdf9b6388464e6b8bad
LaupWing <loc_strict@hotmail.com>
2023-04-14 08:28:06 +0200

    products migration

--- database/migrations/2023_04_12_091442_create_products_table.php
+++ database/migrations/2023_04_12_091442_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
    {
       Schema::create("products", function (Blueprint $table) {
          $table->id();
+         $table->foreignId("store_id")->constrained()->onDelete("cascade");
+         $table->string("name");
+         $table->text("description")->nullable();
+         $table->decimal("price");
+         $table->bigInteger("quantity");
          $table->timestamps();
       });
    }
